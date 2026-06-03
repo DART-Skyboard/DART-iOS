7235d1372da3fb476b3e6534ec073e3b58732b14
Justin Venable <dartsolarpunk@gmail.com>
2026-06-03 16:33:38 -0400

    init: URLHelper.swift

---
+++ Sources/DARTApp/URLHelper.swift
@@ -0,0 +1,12 @@
+
+
+import Foundation
+
+extension URL {
+    func withQuery(_ query: [String: String]) -> URL? {
+        // adds query to URL in correct format
+        var components = URLComponents(url: self, resolvingAgainstBaseURL: true)
+        components?.queryItems = query.map { URLQueryItem(name: $0.0, value: $0.1) }
+        return components?.url
+    }
+}
